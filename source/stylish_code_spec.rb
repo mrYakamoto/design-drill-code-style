2d11eee7f60720c08aa2a2ea7d4654f2a2186729
Lauren Scott & Matt Baker <pair+devdame.mattbaker@devbootcamp.com>
2014-06-02 16:28:58 -0500

    rspec conversion

---
+++ source/stylish_code_spec.rb
@@ -0,0 +1,33 @@
+require_relative('stylish_code')
+
+describe Guessing_game do
+  let(:game) { Guessing_game.new(10) }
+
+  describe '#guess' do
+    it 'should know if a guess is too low' do
+      expect(game.guess(5)).to eq(:low)
+    end
+
+    it 'should know if a guess is too high' do
+      expect(game.guess(15)).to eq(:high)
+    end
+  end
+
+  describe '#solved?' do
+    it 'should return true if the puzzle has been solved' do
+      game.guess(10)
+      expect(game.solved?).to be(true)
+    end
+
+    it 'should return false if the puzzle has not been solved' do
+      expect(game.solved?).to be(false)
+    end
+  end
+
+  context 'with a number greater than 100' do
+    it 'should throw raise an error' do
+      expect { Guessing_game.new(200)}
+        .to raise_exception(RuntimeError, "Answer must be between 1 and 100")
+    end
+  end
+end
